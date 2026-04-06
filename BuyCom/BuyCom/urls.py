128350fd6e012f900fced2b25bca57ccb6c19692
Sharandeep-Singh <sharandeep.singh@nibespace.com>
2026-04-06 11:33:40 +0530

    19th

--- BuyCom/BuyCom/urls.py
+++ BuyCom/BuyCom/urls.py
@@ -25,7 +25,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django-admin/', admin.site.urls),
 
     # Your API routes
     path('', include('main.urls')),
