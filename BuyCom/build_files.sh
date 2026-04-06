3a9deced255dc0ebb485ace987277f7c529ff9f8
Sharandeep-Singh <sharandeep.singh@nibespace.com>
2026-04-06 10:40:20 +0530

    11th

--- BuyCom/build_files.sh
+++ BuyCom/build_files.sh
@@ -2,10 +2,8 @@
 
 echo "🚀 BUILD START"
 
-# Install dependencies from root requirements.txt
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --noinput --clear
 
 echo "✅ BUILD END"
