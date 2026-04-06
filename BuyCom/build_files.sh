63588a88ecf811cc0587f20087ddbb4620f6c012
Sharandeep-Singh <sharandeep.singh@nibespace.com>
2026-04-06 10:31:13 +0530

    8th

--- BuyCom/build_files.sh
+++ BuyCom/build_files.sh
@@ -2,10 +2,8 @@
 
 echo "BUILD START"
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Collect static files (very important for your admin panel CSS/JS)
 python manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
