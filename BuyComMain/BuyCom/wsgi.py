294f9107b33d47086971f801f29e98e2e495686f
Sharandeep-Singh <sharandeep.singh@nibespace.com>
2026-04-27 10:34:23 +0530

    2nd commit000093

--- BuyComMain/BuyCom/wsgi.py
+++ BuyComMain/BuyCom/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
+
 import os
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BuyCom.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BuyCom.settings")
 
-application = get_asgi_application()
-
-
+application = get_wsgi_application()
