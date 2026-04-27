ab669b9ee44b9af4e3181cada384629fafe575c0
Sharandeep-Singh <sharandeep.singh@nibespace.com>
2026-04-27 10:54:49 +0530

    2nd commit000095

--- BuyComMain/main/admin.py
+++ BuyComMain/main/admin.py
@@ -61,11 +61,3 @@ class ContactInquiryAdmin(admin.ModelAdmin):
     list_filter = ["is_read", "created_at"]
     search_fields = ["name", "email", "message"]
     readonly_fields = ["created_at"]
-
-
-@admin.register(ContactInquiry)
-class ContactInquiryAdmin(admin.ModelAdmin):
-    list_display = ["name", "email", "company", "created_at", "is_read"]
-    list_filter = ["is_read", "created_at"]
-    search_fields = ["name", "email", "message"]
-    readonly_fields = ["created_at"]
